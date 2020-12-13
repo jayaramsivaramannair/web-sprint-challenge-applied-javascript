e32f782f44c64ffc8983657e8fb51ab153383955
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2020-12-13 06:59:41 -0500

    Built Header Function in Header.js and Added the Resultant component to the header container

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,26 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    let headerContainer = document.createElement('div');
+    let dateContainer = document.createElement('span');
+    let mainTitle = document.createElement('h1');
+    let tempContainer = document.createElement('span');
+
+    headerContainer.classList.add('header');
+    dateContainer.classList.add('date');
+    dateContainer.textContent = "March 28, 2020";
+    mainTitle.textContent = "Lambda Times";
+    tempContainer.classList.add('temp');
+    tempContainer.textContent = "98°";
+
+    headerContainer.appendChild(dateContainer);
+    headerContainer.appendChild(mainTitle);
+    headerContainer.appendChild(tempContainer);
+
+    return headerContainer;
+}
+
+let headerContainer = document.querySelector('div.header-container');
+console.log(headerContainer);
+headerContainer.appendChild(Header());
