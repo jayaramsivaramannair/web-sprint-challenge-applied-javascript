1c7c84a1a9173e618687139eb291f0a4c6e27021
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2020-12-13 07:16:14 -0500

    Created Tabs using Axios Get Request and Added Each Tab to the DOM

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,18 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+let topicsContainer = document.querySelector('div.topics');
+
+axios.get("https://lambda-times-api.herokuapp.com/topics")
+.then((response) => {
+   console.log(response.data.topics);
+   let topics = response.data.topics;
+   topics.forEach((datum) => {
+       let tab = document.createElement('div');
+       tab.classList.add('tab');
+       tab.textContent = datum;
+       console.log(tab);
+       topicsContainer.appendChild(tab);
+   })
+});
